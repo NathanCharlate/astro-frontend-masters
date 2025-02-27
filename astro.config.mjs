c3d1051c2b2206381feb8d52245d94a4871e87e9
NathanCharlate <ncharlate@gmail.com>
2025-02-27 09:27:29 +0100

    astro course done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import solid from '@astrojs/solid-js';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-frontend-masters.netlify.app',
+
+  integrations: [
+      react({
+          include: ['**/react/*'],
+      }),
+      solid({
+          include: ['**/solid/*'],
+      }),
+	],
+
+  adapter: netlify(),
+});
